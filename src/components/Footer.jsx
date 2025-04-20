826904b4add7ad7ef63971dd46dffe16ad2caad9
AtharvaGGourshete <goursheteatharva@gmail.com>
2025-04-20 22:14:27 +0530

    Update footer copyright year and modify default search city in Weather component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className='footer'>
-        All rights reserved &copy;Atharva Gourshete 2023
+        All rights reserved &copy;Atharva Gourshete 2025
     </div>
   )
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -71,7 +71,7 @@ const Weather = () => {
   };
 
   useEffect(() => {
-    search("London");
+    search("Mumbai");
   }, []);
 
   return (
